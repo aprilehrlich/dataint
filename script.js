09eb0c0380200aa7a54df31c50db1bd5c6a59356
aprjoy <anjoyner@gmail.com>
2017-06-19 19:54:08 -0400

    CSS changes
    changed CSS to center everything on page, cleaned up repetition, added wrapper to HTML, changed JS to reflect new IDs in HTML/CSS

--- script.js
+++ script.js
@@ -30,55 +30,55 @@ $("img").click(function(){
 	var fadeTime = 1000;
 	
 	$("h1").fadeOut(0);
-	$(".image1").fadeTo(fadeTime,0)
+	$("#image1").fadeTo(fadeTime,0)
 		
 	if(currentQuote == quote1){
 		$("h1").text(quote2);
-		$(".image2").fadeTo(fadeTime,1)
+		$("#image2").fadeTo(fadeTime,1)
 	} 
 
 	if(currentQuote == quote2){
 		$("h1").text(quote3);
-		$(".image3").fadeTo(fadeTime,1)
+		$("#image3").fadeTo(fadeTime,1)
 	} 
 
 
 	if(currentQuote == quote3){
 		$("h1").text(quote4);
-		$(".image4").fadeTo(fadeTime,1)
+		$("#image4").fadeTo(fadeTime,1)
 	} 
 
 	if(currentQuote == quote4){
 		$("h1").text(quote5);
-		$(".image5").fadeTo(fadeTime,1)
+		$("#image5").fadeTo(fadeTime,1)
 	} 
 
 
 	if(currentQuote == quote5){
 		$("h1").text(quote6);
-		$(".image6").fadeTo(fadeTime,1)
+		$("#image6").fadeTo(fadeTime,1)
 	} 
 
 
 	if(currentQuote == quote6){
 		$("h1").text(quote7);
-		$(".image7").fadeTo(fadeTime,1)
+		$("#image7").fadeTo(fadeTime,1)
 	} 
 
 
 	if(currentQuote == quote7){
 		$("h1").text(quote8);
-		$(".image8").fadeTo(fadeTime,1)
+		$("#image8").fadeTo(fadeTime,1)
 	} 
 
 	if(currentQuote == quote8){
 		$("h1").text(quote9);
-		$(".image9").fadeTo(fadeTime,1)
+		$("#image9").fadeTo(fadeTime,1)
 	} 
 
 	if(currentQuote == quote9){
 		$("h1").text(quote10);
-		$(".image10").fadeTo(fadeTime,1)
+		$("#image10").fadeTo(fadeTime,1)
 	} 
 
 	$("h1").fadeIn(1000);
